e6c4af64c6e6ab597f5463334d85bcb2ad941568
Michael Leblanc <mleblanc1@werfen.com>
2023-10-31 13:10:37 -0400

    added index files for the controllers and started working on routes

--- models/index.js
+++ models/index.js
@@ -1,1 +1,27 @@
-const Comment = require()
+const Comment = require('./comments');
+const Post = require('./posts');
+const User = require('./users');
+
+// User can have many posts
+User.hasMany(Post, {
+    foreignKey : 'user_id',
+    targetKey: 'id'
+  });
+
+// Posts have a direct relationship with the user that posts them
+Post.belongsTo(User, { 
+    foreignKey: 'user_id',
+    targetKey: 'id'
+});
+
+// Comment belongs to post
+Comment.belongsTo(Post, { 
+    foreignKey: 'comment_id',
+    targetKey: 'id'
+});
+
+module.exports = {
+    Comment,
+    Post,
+    User,
+};
